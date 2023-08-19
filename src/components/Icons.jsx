c8e6c604a6213213581e5de2506f06b6c6f2eb1b
José Esteves <joseesteves199930@gmail.com>
2023-08-18 22:13:43 -0400

    feat: :sparkles: pack de iconos
    Se arregló el pack de iconos que se usará para los módulos y el sistema. Todavía no están todos los iconos, pero se comprobará si habrá más. Se creó el componente para usarlo. El componente tiene una propiedad type. Dependiendo de las opciones module o system, se podrá usar un pack de iconos u otro.

---
+++ src/components/Icons.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+const Icons = ({ className, iconName, type = 'system' }) => {
+	const [fileIcon, setFileIcon] = useState(null);
+	useEffect(() => {
+		import(`./../assets/icons/${type}.svg`)
+			.then(res => setFileIcon(res.default))
+			.catch(err =>
+				console.error(`No se pudo cargar el icono de ${type}. Error: ${err}`),
+			);
+	}, [type]);
+	return (
+		<svg className={className}>
+			<use href={`${fileIcon}#${iconName}`}></use>
+		</svg>
+	);
+};
+export default Icons;

bc5ef8953f90eba2ffe0f49406755d1d0567ca08
José Esteves <joseesteves199930@gmail.com>
2023-09-24 20:21:36 -0400

    style: :lipstick: box
    eslilos de caja por defecto en card module
    
    #	modified:   src/components/CardModule.jsx #	modified:   src/components/style/cardModule.css

--- src/components/CardModule.jsx
+++ src/components/CardModule.jsx
@@ -4,7 +4,7 @@ import Icons from './Icons';
 import './style/cardModule.css';
 const CardModule = ({ title, iconName, to }) => {
 	return (
-		<article className='card'>
+		<article className='box card'>
 			<h2 className='card__title'>{title}</h2>
 			<Icons className='card__icon' iconName={iconName} type='module' />
 			<Link className='card__link' to={to}>

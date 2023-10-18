41b9277e09969458d2a0022c9a951918af4ccb71
José Esteves <joseesteves199930@gmail.com>
2023-10-18 03:17:02 -0400

    lease enter the commit message for your changes. Lines starting

--- src/hooks/usePost.jsx
+++ src/hooks/usePost.jsx
@@ -1,32 +1,38 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useContext, useState } from 'react';
+import { ContextToken } from '../Context/Token.context';
 import { fetchData } from '../helpers/fetch';
 const usePost = () => {
 	const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(true);
 	const [data, setData] = useState({});
-	const handelFetch = useCallback(({ url, form }) => {
-		setLoading(true);
-		fetchData()
-			.post({
-				url,
-				options: {
-					headers: {
-						'Content-type': 'application/json',
+	const { token } = useContext(ContextToken);
+	const handelFetch = useCallback(
+		({ url, form }) => {
+			setLoading(true);
+			fetchData()
+				.post({
+					url,
+					options: {
+						headers: {
+							'Content-type': 'application/json',
+							authorization: `Bearer ${token}`,
+						},
+						body: form,
 					},
-					body: form,
-				},
-			})
-			.then(res => {
-				if (res.errors) {
-					return setError(res.errors);
-				}
-				if (res) {
-					return setData(res);
-				}
-			})
-			.catch(err => console.error(err))
-			.finally(() => setLoading(false));
-	}, []);
+				})
+				.then(res => {
+					if (res.errors) {
+						return setError(res.errors);
+					}
+					if (res) {
+						return setData(res);
+					}
+				})
+				.catch(err => console.error(err))
+				.finally(() => setLoading(false));
+		},
+		[token],
+	);
 	return { data, error, loading, handelFetch };
 };
 export default usePost;

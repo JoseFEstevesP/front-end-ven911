82fb27b20f8d86dce8751471929e9f9cf719ddfe
José Esteves <joseesteves199930@gmail.com>
2023-10-21 23:23:17 -0400

    feat: :sparkles: useo del componente page
    se uso el componente de page en la pagina d eredirección
    
    #	modified:   src/page/Redirect.jsx

--- src/page/Redirect.jsx
+++ src/page/Redirect.jsx
@@ -1,8 +1,10 @@
+import Page from '../components/Page';
+
 const Redirect = () => {
 	return (
-		<>
+		<Page title='redirect'>
 			<h2>Redirect</h2>
-		</>
+		</Page>
 	);
 };
 export default Redirect;

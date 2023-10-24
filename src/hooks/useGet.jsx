d89d5cfa41306628573dc02cc32d385dd1432e6d
José Esteves <joseesteves199930@gmail.com>
2023-10-24 02:37:50 -0400

    refactor: :recycle: renombre de carpeta
    se ronombro la carpeta de context
    
    #	modified:   src/hooks/useExit.jsx #	modified:   src/hooks/useGet.jsx #	modified:   src/hooks/useUpdateData.jsx

--- src/hooks/useGet.jsx
+++ src/hooks/useGet.jsx
@@ -1,9 +1,9 @@
 import { useCallback, useContext, useState } from 'react';
-import { ContextToken } from '../Context/Token.context';
+import { ContextToken } from '../context/Token.context';
 import { system } from '../data/system';
 import { fetchData } from '../helpers/fetch';
 const useGet = () => {
-	const [error, setError] = useState({});
+	const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(true);
 	const [controller, setController] = useState(null);
 	const [data, setData] = useState(null);

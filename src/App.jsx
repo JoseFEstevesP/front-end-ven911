f2f27ab742ef28a9d8a8497a6f6eaa45df68d27d
José Esteves <joseesteves199930@gmail.com>
2023-11-12 21:31:37 -0400

    build: :arrow_up: actualización de dependencias
    se actuaizaron las dependencias
    
    #	modified:   package.json #	modified:   pnpm-lock.yaml #	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Footer from './components/Footer';
 import Head from './components/Head';
 import Msg from './components/Msg';
 import ProtectedRoutes from './components/ProtectedRoutes';
-import RolProvider from './context/Rol.context';
+import SiteProvider from './context/Site.context';
 import TokenProvider from './context/Token.context';
+import RolProvider from './context/Rol.context';
 import MsgProvider from './context/msg.context';
 import Error404 from './page/Error404';
 import Home from './page/Home';
 import ModuleGA from './page/ModuleGA';
 import Redirect from './page/Redirect';
-import SiteProvider from './context/Site.context';
 
 const App = () => {
 	return (
